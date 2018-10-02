1888b80ffd83e48b31a9d58b73a75158009531d1
CedricBour <cedric.bour@hotmail.fr>
2018-10-02 21:48:04 +0200

    [Add] First Commit
    This is the first commit of the activity 2 from Apprendre le JavaScript on OpenClassrooms.

---
+++ js/contacts.js
@@ -0,0 +1,19 @@
+/* 
+Activité : gestion des contacts
+*/
+
+// TODO : complétez le programme
+
+/* VARIABLES*/
+var user = {
+    // initialization
+    init: function (lastName, firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+};
+
+
+/* PROGRAM */
+
+console
